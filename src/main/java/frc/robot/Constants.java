70a9390d8514f2031a11318e5f8a71febbfb9e14
Zachary Holly <zholly18@outlook.com>
2025-02-12 08:25:07 -0800

    EoD 2/11

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -58,7 +58,7 @@ public final class Constants {
 
     public static final int kFrontLeftTurningCanId = 7;
     public static final int kRearLeftTurningCanId = 5;
-    public static final int kFrontRightTurningCanId = 1;
+    public static final int kFrontRightTurningCanId = 19;
     public static final int kRearRightTurningCanId = 3;
 
     public static final int kGyroID = 11;
