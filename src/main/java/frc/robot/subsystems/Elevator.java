c9b2d33fb2fd5707197156bda3e540e0d0cdfc2d
sf3990 <134634823+yanchingf@users.noreply.github.com>
2025-02-25 15:51:18 -0800

    a

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -16,6 +16,7 @@ import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import frc.robot.Configs;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
@@ -26,7 +27,7 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 public class Elevator extends SubsystemBase {
 
     static SparkMax m_elevator = new SparkMax(DriveConstants.elevatorCANId, MotorType.kBrushless);
-    public static final RelativeEncoder m_elevatorencoder = m_elevator.getEncoder(); 
+    public static final AbsoluteEncoder m_elevatorencoder = m_elevator.getEncoder(); 
     static SparkClosedLoopController m_elevatorController = m_elevator.getClosedLoopController();
     private static double elevatorCurrentTarget = ElevatorPoints.lvl_one;
 
